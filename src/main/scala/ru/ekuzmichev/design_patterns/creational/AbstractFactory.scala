6154bc7412a1fdc79df09e14f2bb088bdd319c13
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2024-11-20 20:14:21 +0300

    Add Abstract factory pattern example

---
+++ src/main/scala/ru/ekuzmichev/design_patterns/creational/AbstractFactory.scala
@@ -0,0 +1,62 @@
+package ru.ekuzmichev.design_patterns.creational
+
+// The purpose is the same as all factory design patterns—to encapsulate the object creation logic
+// and hide it from the user. The difference is how it is implemented.
+//
+//The abstract factory design pattern relies on object composition in contrast to inheritance,
+// which is used by the factory method. Here, we have a separate object, which provides
+// an interface to create instances of the classes we need.
+
+object AbstractFactory {
+  trait SimpleConnection {
+    def getName(): String
+
+    def executeQuery(query: String): Unit
+  }
+
+  class SimpleMysqlConnection extends SimpleConnection {
+    override def getName(): String = "SimpleMysqlConnection"
+
+    override def executeQuery(query: String): Unit = {
+      System.out.println(s"Executing the query '$query' the MySQL way.")
+    }
+  }
+
+  class SimplePgSqlConnection extends SimpleConnection {
+    override def getName(): String = "SimplePgSqlConnection"
+
+    override def executeQuery(query: String): Unit = {
+      System.out.println(s"Executing the query '$query' the PgSQL way.")
+    }
+  }
+
+  trait DatabaseConnectorFactory {
+    def connect(): SimpleConnection
+  }
+
+  class MySqlFactory extends DatabaseConnectorFactory {
+    override def connect(): SimpleConnection = new SimpleMysqlConnection
+  }
+
+  class PgSqlFactory extends DatabaseConnectorFactory {
+    override def connect(): SimpleConnection = new SimplePgSqlConnection
+  }
+
+  class DatabaseClient(connectorFactory: DatabaseConnectorFactory) {
+    def executeQuery(query: String): Unit = {
+      val connection = connectorFactory.connect()
+      connection.executeQuery(query)
+    }
+  }
+}
+
+object AbstractFactoryExample {
+  import AbstractFactory._
+
+  def main(args: Array[String]): Unit = {
+    val clientMySql: DatabaseClient = new DatabaseClient(new MySqlFactory)
+    val clientPgSql: DatabaseClient = new DatabaseClient(new PgSqlFactory)
+    clientMySql.executeQuery("SELECT * FROM users")
+    clientPgSql.executeQuery("SELECT * FROM employees")
+  }
+}
--- src/main/scala/ru/ekuzmichev/design_patterns/creational/FactoryMethod.scala
+++ src/main/scala/ru/ekuzmichev/design_patterns/creational/FactoryMethod.scala
@@ -11,47 +11,51 @@ package ru.ekuzmichev.design_patterns.creational
 // This design pattern is also helpful when object creation would
 // otherwise require extra parameters to be passed by the developer.
 
-trait SimpleConnection {
-  def getName(): String
+object FactoryMethod {
+  trait SimpleConnection {
+    def getName(): String
 
-  def executeQuery(query: String): Unit
-}
+    def executeQuery(query: String): Unit
+  }
 
-class SimpleMysqlConnection extends SimpleConnection {
-  override def getName(): String = "SimpleMysqlConnection"
+  class SimpleMysqlConnection extends SimpleConnection {
+    override def getName(): String = "SimpleMysqlConnection"
 
-  override def executeQuery(query: String): Unit = {
-    System.out.println(s"Executing the query '$query' the MySQL way.")
+    override def executeQuery(query: String): Unit = {
+      System.out.println(s"Executing the query '$query' the MySQL way.")
+    }
+  }
+
+  class SimplePgSqlConnection extends SimpleConnection {
+    override def getName(): String = "SimplePgSqlConnection"
+
+    override def executeQuery(query: String): Unit = {
+      System.out.println(s"Executing the query '$query' the PgSQL way.")
+    }
+  }
+
+  trait DatabaseClient {
+    def executeQuery(query: String): Unit = {
+      val connection = connect()
+      connection.executeQuery(query)
+    }
+
+    // Factory method
+    protected def connect(): SimpleConnection
+  }
+
+  class MysqlClient extends DatabaseClient {
+    override protected def connect(): SimpleConnection = new SimpleMysqlConnection
+  }
+
+  class PgSqlClient extends DatabaseClient {
+    override protected def connect(): SimpleConnection = new SimplePgSqlConnection
   }
 }
 
-class SimplePgSqlConnection extends SimpleConnection {
-  override def getName(): String = "SimplePgSqlConnection"
+object FactoryMethodExample {
+  import FactoryMethod._
 
-  override def executeQuery(query: String): Unit = {
-    System.out.println(s"Executing the query '$query' the PgSQL way.")
-  }
-}
-
-trait DatabaseClient {
-  def executeQuery(query: String): Unit = {
-    val connection = connect()
-    connection.executeQuery(query)
-  }
-
-  // Factory method
-  protected def connect(): SimpleConnection
-}
-
-class MysqlClient extends DatabaseClient {
-  override protected def connect(): SimpleConnection = new SimpleMysqlConnection
-}
-
-class PgSqlClient extends DatabaseClient {
-  override protected def connect(): SimpleConnection = new SimplePgSqlConnection
-}
-
-object Example {
   def main(args: Array[String]): Unit = {
     val clientMySql: DatabaseClient = new MysqlClient
     clientMySql.executeQuery("SELECT * FROM users")
